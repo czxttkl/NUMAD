82cb624910f8024c9d44c2f6c7da668c8d4636aa
czxttkl <czxttkl@gmail.com>
2013-11-06 12:38:44 -0500

    git init

---
+++ src/edu/neu/mhealth/debug/CameraActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.mhealth.debug;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CameraActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+	}
+
+	
+}
