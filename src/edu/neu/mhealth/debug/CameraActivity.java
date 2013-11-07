aa359d3699d11e766099f5f2ee55968a88bfb2d4
czxttkl <czxttkl@gmail.com>
2013-11-06 21:42:44 -0500

    implement CvCameraViewListener2 for CameraActivity

--- src/edu/neu/mhealth/debug/CameraActivity.java
+++ src/edu/neu/mhealth/debug/CameraActivity.java
@@ -2,9 +2,12 @@ package edu.neu.mhealth.debug;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
 import edu.neu.mhealth.debug.helper.Global;
 
@@ -12,8 +15,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements CvCameraViewListener2 {
 
 	/*Basic Variables*/
 	private final String TAG = Global.APP_LOG_TAG;
@@ -36,11 +41,19 @@ public class CameraActivity extends Activity {
 			}
 		}
 	};
-
+	
+	/*
+	 *   Activity Callbacks
+	 *   
+	 *   */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		setContentView(R.layout.activity_camera);
+		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HellpOpenCvView);
+		mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+		mOpenCvCameraView.setCvCameraViewListener(this);
 	}
 
 	@Override
@@ -59,6 +72,30 @@ public class CameraActivity extends Activity {
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
+	}
+
+	
+	
+	/*
+	 *   Opencv Callbacks
+	 *   
+	 *   */
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCameraViewStopped() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	
