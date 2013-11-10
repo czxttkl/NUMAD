a491d7b612b6286d4b5402140a3e3aa59fd95c47
czxttkl <czxttkl@gmail.com>
2013-11-09 19:33:56 -0500

    reliable vertion real-time object detection

--- src/edu/neu/mhealth/debug/CameraActivity.java
+++ src/edu/neu/mhealth/debug/CameraActivity.java
@@ -16,6 +16,7 @@ import org.opencv.core.Core.MinMaxLocResult;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
@@ -37,16 +38,22 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 	/* Basic Variables */
 	private final String TAG = Global.APP_LOG_TAG;
 	private long cameraStartTime;
-	private static final Scalar RECT_COLOR = new Scalar(0, 255, 0, 255);
+	private static final Scalar RECT_COLOR_RED = new Scalar(255, 0, 0, 255);
+	private static final Scalar RECT_COLOR_GREEN = new Scalar(0, 255, 0, 255);
 
 	/* OpenCv Variables */
 	private Mat mRgba;
 	private Mat toBeDetectedMat;
 	private Mat detectResult;
 	private Mat nullMat;
+	
+	private Rect captureRect;
+	private Point captureRectPt1;
+	private Point captureRectPt2;
+	
 	private boolean srcCaptured = false;
-	private int resultRows;
-	private int resultCols;
+	private int resultMatRows;
+	private int resultMatCols;
 	private CameraBridgeViewBase mOpenCvCameraView;
 	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 		@Override
@@ -106,85 +113,72 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 	 */
 	@Override
 	public void onCameraViewStarted(int width, int height) {
+		//Initialize Mats which will be used in onCameraFrame
+		toBeDetectedMat = new Mat();
+		nullMat = new Mat();
+		
 		cameraStartTime = System.currentTimeMillis();
-		// Bitmap toBeDetectedBitmap = null;
-		// try {
-		// toBeDetectedBitmap =
-		// BitmapFactory.decodeStream(getAssets().open("shoes.jpg"));
-		// toBeDetectedMat = new Mat();
-		// Utils.bitmapToMat(toBeDetectedBitmap, toBeDetectedMat);
-		// Log.e(TAG, "load detect object image success. Mat:" +
-		// toBeDetectedMat.rows() + "*" + toBeDetectedMat.cols());
-		// } catch (IOException e) {
-		// Log.e(TAG, "load detect object image failed");
-		// }
-
+		
+		//Capture area is 200*200
+		captureRectPt1 = new Point(width/2 - 100, height/2 - 100);
+		captureRectPt2 = new Point(width/2 + 100, height/2 + 100);
+		captureRect = new Rect(captureRectPt1, captureRectPt2);
+	
+		resultMatRows = height - 200 + 1;
+		resultMatCols = width - 200 + 1;
+		
+		Log.i(TAG, "onCameraViewStarted:" + width + "*" + height);
 	}
 
 	@Override
 	public void onCameraViewStopped() {
-
+		Imgproc.cvtColor(toBeDetectedMat, toBeDetectedMat,
+				Imgproc.COLOR_BGRA2RGBA);
+		File path = Environment
+				.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+		String filename = "captured1.png";
+		File file = new File(path, filename);
+		filename = file.toString();
+		Highgui.imwrite(filename, toBeDetectedMat);
 	}
 
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		//It's actually bgra mat!
 		mRgba = inputFrame.rgba();
+		
 		if (System.currentTimeMillis() - cameraStartTime < 5000) {
-			// Point pt1 = new Point(210, 240);
-			// Point pt2 = new Point(420, 480);
-			// Core.rectangle(mRgba, pt1, pt2, RECT_COLOR, 3);
+			 Core.rectangle(mRgba, captureRectPt1, captureRectPt2, RECT_COLOR_RED, 3);
 		} else {
 			if (!srcCaptured) {
-//				toBeDetectedMat = mRgba.submat(240, 480, 210, 420);
-//				Imgproc.cvtColor(toBeDetectedMat, toBeDetectedMat,
-//						Imgproc.COLOR_BGRA2RGBA);
-				srcCaptured = true;
-				nullMat = new Mat();
-
+				mRgba.submat(captureRect).copyTo(toBeDetectedMat);
+				Mat toBeDetectedMatRGBA = new Mat();
+				Imgproc.cvtColor(toBeDetectedMat, toBeDetectedMatRGBA, Imgproc.COLOR_BGRA2RGBA);
 				File path = Environment
 						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 				String filename = "captured.png";
 				File file = new File(path, filename);
 				filename = file.toString();
-//				toBeDetectedMat = Highgui.imread(filename, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-				toBeDetectedMat = Highgui.imread(filename, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-				
-				resultRows = mRgba.rows() - toBeDetectedMat.rows() + 1;
-				resultCols = mRgba.cols() - toBeDetectedMat.cols() + 1;
-				detectResult = new Mat(resultRows, resultCols, CvType.CV_32FC1);
-				// boolean fileSaveResult = Highgui.imwrite(filename,
-				// toBeDetectedMat);
-				//
-				// Log.i(TAG, "save file to " + filename + "?" +
-				// fileSaveResult);
-				//
-				// filename = "whole.png";
-				// file = new File(path, filename);
-				// filename = file.toString();
-				// Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_BGRA2RGBA);
-				//
-				// fileSaveResult = Highgui.imwrite(filename, toBeDetectedMat);
-
+				Highgui.imwrite(filename, toBeDetectedMatRGBA);
+//				toBeDetectedMat = Highgui.imread(filename, Highgui.CV_LOAD_IMAGE_GRAYSCALE);		
+				detectResult = new Mat(resultMatRows, resultMatCols, CvType.CV_32FC1);
+				srcCaptured = true;
 			} else {
-				Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGBA2GRAY);
-				detectResult.create(resultRows, resultCols, CvType.CV_32FC1);
+				// Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGBA2GRAY);
+				// Create result mat
+				detectResult.create(resultMatRows, resultMatCols, CvType.CV_32FC1);
 				// Choose one method for template match
 				int matchMethod = Imgproc.TM_SQDIFF_NORMED;
-				// Create result mat
 				// Match template
 				Imgproc.matchTemplate(mRgba, toBeDetectedMat, detectResult,
 						matchMethod);
 				// Normalize
-				Core.normalize(detectResult, detectResult, 0, 1,
-						Core.NORM_MINMAX, -1, nullMat);
+				Core.normalize(detectResult, detectResult, 0, 1, Core.NORM_MINMAX, -1, nullMat);
 				// Create a point for match location
 				Point matchLoc;
-				MinMaxLocResult minMaxLocResult = Core.minMaxLoc(detectResult,
-						nullMat);
-
-				// For SQDIFF and SQDIFF_NORMED, the best matches are lower
-				// values.
-				// For all the other methods, the higher the better
+				MinMaxLocResult minMaxLocResult = Core.minMaxLoc(detectResult, nullMat);
+				// For SQDIFF and SQDIFF_NORMED, the best matches are lower values.
+				// For all the other methods, the higher the better.
 				if (matchMethod == Imgproc.TM_SQDIFF
 						|| matchMethod == Imgproc.TM_SQDIFF_NORMED) {
 					matchLoc = minMaxLocResult.minLoc;
@@ -195,11 +189,8 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 				// Start draw rectangle
 				Point pt2 = new Point(matchLoc.x + toBeDetectedMat.cols(),
 						matchLoc.y + toBeDetectedMat.rows());
-				// Log.i(TAG, detectResult.get(209, 2, data))
-				// Log.i(TAG, "point1:" + matchLoc.x + "," + matchLoc.y
-				// + "  point2:" + pt2.x + "," + pt2.y);
-				Core.rectangle(mRgba, matchLoc, pt2, RECT_COLOR, 3);
-
+				Core.rectangle(mRgba, matchLoc, pt2, RECT_COLOR_GREEN, 3);
+				//Release detectResult 
 				detectResult.release();
 			}
 		}
